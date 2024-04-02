856834cfecbf8e36b2b6ad4bcfa3f35ed50ed6b6
Piyush Kumar Singh <21bcs1638@cuchd.in>
2024-04-02 18:27:11 +0530

    deleted export unnecessary

--- src/app/sell/page.tsx
+++ src/app/sell/page.tsx
@@ -3,14 +3,7 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { MdOutlineMyLocation } from "react-icons/md";
 
-export function InputWithLabel() {
-  return (
-    <div className="grid w-full max-w-sm items-center gap-1.5">
-      <Label htmlFor="email">Email</Label>
-      <Input type="email" id="email" placeholder="Email" />
-    </div>
-  );
-}
+
 
 const page = () => {
   return (
